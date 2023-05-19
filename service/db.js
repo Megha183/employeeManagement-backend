ebd26bcb72ed0d75396d650ade6d9fad8cac3897
unknown <meghabhaskar183@gmail.com>
2023-05-19 12:20:32 +0530

    initial commit

---
+++ service/db.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+
+mongoose.connect('mongodb://127.0.0.1:27017/ems')
+
+// model
+const Employee=mongoose.model('Employee',{
+    id:String,
+    uname:String,
+    age:String,
+    designation:String,
+    salary:Number
+})
+
+module.exports={
+    Employee
+}
---
+++ service/logic.js
@@ -0,0 +1,109 @@
+const db=require('./db')
+
+const allEmployee=()=>{
+    return db.Employee.find().then(result=>{
+        if(result){
+            return {
+                statusCode:200,
+                employees:result
+            }
+        }
+        else{
+            return{
+                statusCode:404,
+                message:"data is not available"
+            }
+        }
+    })
+}
+const addEmployee=(id,uname,age,designation,salary)=>{
+   return db.Employee.findOne({id}).then(result=>{
+        if(result){
+            return{
+                statusCode:404,
+                message:"employee already exist"
+            }
+        }
+        else{
+            // create object of employee model for new employee
+            
+                const newEmp=new db.Employee({
+                    id,
+                    uname,
+                    age,
+                    designation,
+                    salary
+                })
+                // save the object in db
+            newEmp.save()
+
+            return {
+                statusCode:200,
+                message:"employee added succesfully"
+                
+            }
+        }
+    })
+}
+
+const removeEmployee=(eid)=>{
+    return db.Employee.deleteOne({id:eid}).then(result=>{
+        if(result){
+            return{
+                statusCode:200,
+                message:"employee removed"
+            }
+        }
+        else{
+            return{
+                statusCode:404,
+                message:"employee not present"
+            }
+        }
+    })
+}
+
+
+const getAnEmp=(id)=>{
+    return db.Employee.findOne({id}).then(result=>{
+        if(result){
+            return{
+                statusCode:200,
+                employee:result
+            }
+        }else{
+            return{
+                statusCode:404,
+                message:"employee not present"
+            }
+        }
+    })
+}
+
+const editEmp=(id,uname,age,desig,salary)=>{
+    return db.Employee.findOne({id}).then(result=>{
+        if(result){
+            result.id=id
+            result.uname=uname
+            result.age=age
+            result.designation=desig
+            result.salary=salary
+
+            result.save()
+            return{
+                statusCode:200,
+                message:"employee data updated"
+            }
+        }
+        else{
+            return{
+            statusCode:404,
+            message:"employee not present"
+            }
+            
+        }
+    })
+}
+module.exports={
+    allEmployee,addEmployee,removeEmployee,getAnEmp,editEmp
+}
